d8ba2fa16c2942e8d7a9862bf0b71a03119cc96b
dorefaeli <dorefaeli@gmail.com>
2019-12-28 15:41:11 +0200

    Adding SymbolTable

---
+++ SymbolTable.cpp
@@ -0,0 +1,91 @@
+//
+// Created by Dor Refaeli on 28/12/2019.
+//
+
+#include "SymbolTable.h"
+
+SymbolTable::SymbolTable() {
+    this->father = nullptr;
+}
+
+SymbolTable::SymbolTable(SymbolTable *father) : father(father) {}
+
+float SymbolTable::getVariable(string name) {
+    if (!this->recursiveContains(name)) {
+        throw "Asked for non existing variable " + name;
+    } else if (this->contains(name)) {
+        return localVariables[name];
+    } else if (this->father != nullptr) {
+        return this->father->getVariable(name);
+    }
+}
+
+void SymbolTable::setVariable(string name, float num) {
+    if (!this->contains(name) && this->recursiveContains(name)) {
+        this->father->setVariable(name, num);
+    } else {
+        this->localVariables[name] = num;
+        this->addToOutgoing(name, num);
+    }
+}
+
+void SymbolTable::setRemoteVariable(string name, string direction, string simLocation) {
+    this->remoteVariables[name] = make_pair(direction, simLocation);
+    if (direction == "<-") {
+        addToIngoing(name, simLocation);
+    }
+}
+
+bool SymbolTable::contains(string name) {
+    return this->localVariables.count(name);
+}
+
+bool SymbolTable::recursiveContains(string name) {
+    if (this->father != nullptr) {
+        return this->contains(name) || this->father->recursiveContains(name);
+    } else {
+        return this->contains(name);
+    }
+}
+
+map<string, string> SymbolTable::getIngoing() {
+    return this->ingoing;
+}
+
+map<string, float> SymbolTable::getOutgoing() {
+    map<string, float> temp;
+    auto it = this->outgoing.begin();
+    while (it != this->outgoing.end()) {
+        temp[it->first] = it->second;
+    }
+    this->outgoing.clear();
+    return temp;
+}
+
+map<string, float> SymbolTable::updatedMap() {
+    map<string, float> temp;
+    if (this->father != nullptr) {
+        temp = this->father->updatedMap();
+    }
+    auto it = this->localVariables.begin();
+    while (it != this->localVariables.end()) {
+        temp[it->first] = it->second;
+    }
+    return temp;
+}
+
+void SymbolTable::addToIngoing(string name, string simLocation) {
+    this->ingoing[name] = simLocation;
+}
+
+void SymbolTable::addToOutgoing(string name, float num) {
+    if (this->father == nullptr) {
+        if (this->remoteVariables.count(name)) {
+            if (this->remoteVariables[name].first == "->") {
+                this->outgoing[this->remoteVariables[name].second] = num;
+            }
+        }
+    } else {
+        this->father->addToOutgoing(name, num);
+    }
+}
---
+++ SymbolTable.h
@@ -0,0 +1,48 @@
+//
+// Created by Dor Refaeli on 28/12/2019.
+//
+
+#ifndef ALGORITHMICPROGRAMMINGPROJECT_SYMBOLTABLE_H
+#define ALGORITHMICPROGRAMMINGPROJECT_SYMBOLTABLE_H
+
+#include <string>
+#include <unordered_map>
+#include <map>
+
+using namespace std;
+
+class SymbolTable {
+    SymbolTable *father;
+    unordered_map<string, pair<string, string> > remoteVariables; // map of <name, pair<direction, simLocation> >
+    unordered_map<string, float> localVariables; // map of <name, value>
+    map<string, string> ingoing;
+    map<string, float> outgoing;
+public:
+    SymbolTable();
+
+    explicit SymbolTable(SymbolTable *father);
+
+    float getVariable(string);
+
+    void setVariable(string, float);
+
+    void setRemoteVariable(string, string, string);
+
+    bool contains(string);
+
+    bool recursiveContains(string);
+
+    map<string, string> getIngoing();
+
+    map<string, float> getOutgoing();
+
+    map<string, float> updatedMap();
+
+private:
+    void addToIngoing(string, string);
+
+    void addToOutgoing(string, float);
+};
+
+
+#endif //ALGORITHMICPROGRAMMINGPROJECT_SYMBOLTABLE_H
